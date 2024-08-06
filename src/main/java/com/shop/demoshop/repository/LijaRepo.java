f115d6764fc11d46d116a9e0d3d0c0db988ff595
NachoVelazquez <nachovelmu1@gmail.com>
2024-08-06 17:45:51 +0200

    repos

---
+++ src/main/java/com/shop/demoshop/repository/LijaRepo.java
@@ -0,0 +1,21 @@
+package com.shop.demoshop.repository;
+
+import com.shop.demoshop.models.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface LijaRepo extends JpaRepository<Tabla, Integer>{
+
+    @Query("select l from Lija l where l.marcas=:marcas")
+    public List<Lija> findByMarcas(@Param("marcas") Marcas marcas);
+
+    @Query("select l from Lija l where l.abrasion=:abrasion")
+    public List<Lija> findByAbrasion(@Param("abrasion") Abrasion abrasion);
+
+}
---
+++ src/main/java/com/shop/demoshop/repository/RuedasRepo.java
@@ -0,0 +1,23 @@
+package com.shop.demoshop.repository;
+
+import com.shop.demoshop.models.Dureza;
+import com.shop.demoshop.models.Ruedas;
+import com.shop.demoshop.models.Tabla;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface RuedasRepo extends JpaRepository<Tabla, Integer>{
+
+    @Query("select r from Ruedas r where r.medida=:medida")
+    public List<Ruedas> findByAnchura(@Param("medida") int medida);
+
+    @Query("select r from Ruedas r where r.dureza=:medida")
+    public List<Ruedas> findByAnchura(@Param("dureza") Dureza dureza);
+
+}
